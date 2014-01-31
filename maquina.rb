a12b013ad9c0d526e3156e89a5988c1c7bfb7671
oswaldo andres jimenez hidalgo <andreso@andreso-Aspire-5733.(none)>
2014-01-31 16:47:45 -0430

    Cambios que no funcionan

--- maquina.rb
+++ maquina.rb
@@ -1,39 +1,104 @@
 #!/usr/bin/env ruby
+$LOAD_PATH << '.'
+include "insumo"
 #*******************************************************************************
 #----------------- IMPLEMENTACION DE LA CLASE MAQUINA --------------------------
 #*******************************************************************************
 class Maquina
 
+    #---------- Variables de instancia de la clase Maquina ------------
+    attr_accessor :nombre, :desecho, :productoAactual, :cantidadMax, :porcentajeMezcla,
+                  :ciclosProcesamiento,:estado, :maquinaAnterior
+    
     #----------- Constructor de la clase Maquina ------------
-    #-- @param nombre {string correspondiente al nombre de la maquina}
-    def initialize(nombre)
-        @nombre=nombre
-        @cantidadMax=cantMax
-        @desecho=desecho
-        @ciclosProc=ciclosProc
-        @estado=estado
-        @maquinaAnterior
-        @maquinaSiguiente
+    #-- @param nombre  {string correspondiente al nombre de la maquina}
+    #-- @param desecho {numero correspondiente al desecho que produce la maquina}
+    #-- @param cantidadMax {numero correspondiente a la cantidad maxima de 
+    #--        insumos soportado por la maquina}
+    #-- @param porcentajeMezcla {numero correspondiente al porcentaje
+    #--        de insumo soportado por de la maquina}
+    #-- @param ciclosProcesamiento {numero correspondiente a los ciclos de
+    #--        procesamiento requeridos por la maquina}
+    #-- @param estado {string correspondiente al estado de la maquina, una
+    #--        maquina recien cerada es inactiva por defecto}
+    #-- @param maquinaAnterior {Maquina.class un apuntador a la maquina previa}
+    def initialize(nombre,desecho,productoActual,cantidadMax,porcentajeMezcla,
+                   ciclosProcesamiento,estado="Inactiva",maquinaAnterior=nil)
+                   
+        #--- Variables de instancia obligatorias al invocar el constructor
+        @nombre   = nombre
+        @desecho  = desecho
+        
+        @productoActual      =  productoActual
+        @cantidadMax         =  cantidadMax
+        @porcentajeMezcla    =  porcentajeMezcla
+        @ciclosProcesamiento =  ciclosProcesamiento
+        
+        #--- Variables de instancia opcionales al invocar el constructor
+        @estado              = estado
+        @maquinaAnterior     =  maquinaAnterior
     end
 
-
-    #--------- Modifica el estado actual de la Maquina -------
-    #-- @param estado {string correspondiente al estado de la maquina}
-    def setEstado(estado)
-        @estado_maquina=estado
+    #---------- Metodo que ejecuta un ciclo de procesamiento ----------
+    def procesamiento
+    
+        case @estado       
+            when "Inactiva"
+                cicloInactiva
+            when "Procesando"
+                cicloProcesamiento
+            when "Espera"
+                cicloEspera
+            when "Llena"
+                cicloLlena
+            when "Llenando"
+                cicloLlenando
+        end
+    
+    end  
+ 
+    #------ Metodo que procesa un ciclo en estado "llenando". -----
+    def cicloLlenando
+        
     end
-
-    #--------- Retorna el estado actual de la Maquina --------
-    #-- @return estado_maquina {string correspondiente al estado de la maquina}
-    def getEstado
-        puts @estado_maquina
+     
+    #------ Metodo que procesa un ciclo en estado "inactiva". -----
+    def cicloInactiva
+        
     end
     
+    #------ Metodo que procesa un ciclo en estado "procesando". -----
+    def cicloProcesamiento
+                
+    end    
+    
+    #------ Metodo que procesa un ciclo en estado "espera". -----
+    def cicloEspera
+        
+    end    
+    
+    #------ Metodo que procesa un ciclo en estado "llena". -----
+    def cicloLlena
+       
+    end    
+    
+       
     #------ Representacion en String de la clase Maquina. -----
     #-- Imprime en pantalla dicha representacion
-    def print
-        puts ("[" + @nombre_maquina + " : " + @estado_maquina+"]")
+    def imprimir
+        maquina = "Maquina <" + @nombre + ">\n" + "Estado: <" + @estado +">\n"
+        
+        #-- Solo se imprimen los insumos asociados a la maquina en caso de 
+        #-- que esta se encuentre en estado inactiva o llena
+        case @estado
+            when "Llena","Inactiva"
+                puts "Insumos:"
+        end
     end
-    
 #--Fin de la clase Maquina
 end
+
+ins = Insumo.new
+maquina = Maquina.new("M1",1,1,1,1,"Espera",ins)
+maquina.imprimir
+maquina.cicloLlena
