bee33220562e4776ed3fa54939361d1d8e4df7bd
Fernando D'agostino <fertaku@atlas.aperture.org>
2014-01-31 22:14:05 -0430

    Agregadas las maquinas siguientes en la creacion d maquinas simples

--- fabrica.rb
+++ fabrica.rb
@@ -1,7 +1,14 @@
+#!/usr/bin/env ruby
+
+#*****************************************************************************
+#-------------------- IMPLEMENTACION DE LA CLASE FABRICA ---------------------
+#*****************************************************************************
 class Fabrica
 
+    # ---- Atributos de la Clase ---- #
     attr_accessor :numCiclos, :cebada, :arrozMaiz, :levadura, :lupulo
 
+    # ---- Constructor de la Clase ---- #
     def initialize(numCiclos, cebada, arrozMaiz, levadura, lupulo)
         @numCiclos = numCiclos
         @cebada    = cebada
@@ -10,26 +17,31 @@ class Fabrica
         @lupulo    = lupulo
     end
 
+    # ---- Metodo para activar la funcionalidad de la Fabrica ---- #
+    # Toma las entradas proporcionadas e imprime la cerveza total y#
+    # tambien las cantidades sobrantes de los insumos particulares #
     def activar
         
         puts "Inicio Planta"
         puts "#{@numCiclos}"
 
-        #FALTA PONER LAS MAQUINAS SIGUIENTES
+        
+        # ---- Se crea cada una de las maquinas ---- #
 
-        silosCebada    = MaquinaCompleja.new
-        molino         = Maquina.new("Molino", 0.02, 0, 100, 100, 1, nil, nil)
-        pailaMezcla    = MaquinaCompleja.new
-        cubaFiltracion = Maquina.new("Cuba de Filtracion", 0.35, 0, 135, 100, 2, nil, nil)
-        pailaCoccion   = MaquinaCompleja.new
-        tanque         = Maquina.new("Tanque Pre-Clarificador", 0.01, 0, 35, 100, 1, nil, nil)
-        enfriador      = Maquina.new("Enfriador", 0.0, 0, 60, 100, 2, nil, nil)
-        tcc            = MaquinaCompleja.new
-        filtroCerveza  = Maquina.new("Filtro de Cerveza", 0.0, 0, 100, 100, 1, nil, nil)
-        tanqueFiltrada = Maquina.new("Tanque para Cerveza Filtrada", 0.0, 0, 100, 100, 0, nil, nil)
         llenaTapa      = Maquina.new("Llenadora y Tapadora", 0.0, 0, 50, 100, 2, nil, nil)
+        tanqueFiltrada = Maquina.new("Tanque para Cerveza Filtrada", 0.0, 0, 100, 100, 0, llenaTapa, nil)
+        filtroCerveza  = Maquina.new("Filtro de Cerveza", 0.0, 0, 100, 100, 1, tanqueFiltrada, nil)
+        tcc            = MaquinaCompleja.new
+        enfriador      = Maquina.new("Enfriador", 0.0, 0, 60, 100, 2, tcc, nil)
+        tanque         = Maquina.new("Tanque Pre-Clarificador", 0.01, 0, 35, 100, 1, enfriador, nil)
+        pailaCoccion   = MaquinaCompleja.new
+        cubaFiltracion = Maquina.new("Cuba de Filtracion", 0.35, 0, 135, 100, 2, pailaCoccion, nil)
+        pailaMezcla    = MaquinaCompleja.new
+        molino         = Maquina.new("Molino", 0.02, 0, 100, 100, 1, pailaMezcla, nil)
+        silosCebada    = MaquinaCompleja.new
 
 
+        # ---- Se ejecutan los ciclos especificados ---- #
         for i in 1..@numCiclos
             silosCebada.procesamiento
             molino.procesamiento
@@ -44,6 +56,7 @@ class Fabrica
             llenaTapa.procesamiento   
         end
 
+        # ---- Se imprimen los resultados ---- #
         puts "Cerveza Total: "
         puts "Cebada Sobrante: "
         puts "Lupulo Sobrante: "
@@ -54,12 +67,14 @@ class Fabrica
 
 end
 
+
+# ---- Se obtienen los argumentos de la linea de comandos ---- #
 numCiclos = ARGV[0].to_i
 cebada    = ARGV[1].to_i
 arrozMaiz = ARGV[2].to_i
 levadura  = ARGV[3].to_i
 lupulo    = ARGV[4].to_i
 
-
+# ---- Se crea la Fabrica y se pone en marcha ---- #
 glaciar = Fabrica.new(numCiclos, cebada, arrozMaiz, levadura, lupulo)
 glaciar.activar
