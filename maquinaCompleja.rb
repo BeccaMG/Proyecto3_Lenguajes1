7374ebcc8dd8d5e1f8bb62891ac83b518053491d
oswaldo andres jimenez hidalgo <andreso@andreso-Aspire-5733.(none)>
2014-01-31 23:59:58 -0430

    maquina

--- maquinaCompleja.rb
+++ maquinaCompleja.rb
@@ -43,53 +43,7 @@ end
 #*******************************************************************************
 #----------------- IMPLEMENTACION DE LA CLASE MAQUINA --------------------------
 #*******************************************************************************
-class Maquina
-
-    #---------- Variables de instancia de la clase Maquina ------------
-    attr_accessor :nombre, :desecho, :productoAnterior, :cantidadMax, :porcentajePA,
-                  :ciclosProcesamiento,:estado, :maquinaAnterior
-    
-    #----------- Constructor de la clase Maquina ------------
-    #-- @param nombre  {string correspondiente al nombre de la maquina}
-    #-- @param desecho {numero correspondiente al desecho que produce la maquina}
-    #-- @param productoAnterior {instancia de Insumo correspondiente al contenedor
-    #-- del Insumo PA de la maquina. A su vez, indica el contenedor en el que
-    #-- la maquina anterior deposita el producto manufacturado
-    #--
-    #-- estado de la maquina, una
-    #--        maquina recien cerada es inactiva por defecto}
-    #-- @param cantidadMax {numero correspondiente a la cantidad maxima de 
-    #--        insumos soportado por la maquina}
-    #-- @param porcentajePA {numero correspondiente al porcentaje
-    #--        de insumo soportado por de la maquina}
-    #-- @param ciclosProcesamiento {numero correspondiente a los ciclos de
-    #--        procesamiento requeridos por la maquina}
-    #-- @param maquinaAnterior {Maquina.class un apuntador a la maquina previa}
-    #-- @param productoProcesado {Instancia de Producto PA correspondiente al
-    #--        contenedor de insumos de la siguiente maquina del proceso. Si es
-    #--        nil se trata de la ultima maquina}
-    def initialize(nombre,desecho,cantidadMax,porcentajePA,ciclosProcesamiento,
-                    maquinaSiguiente,productoAnterior,productoProcesado)
-                   
-        #--- Variables de instancia obligatorias al invocar el constructor
-        @nombre   = nombre
-        @desecho  = desecho
-
-        @cantidadMax         =  cantidadMax
-        @porcentajePA        =  porcentajePA
-        @ciclosProcesamiento =  ciclosProcesamiento
-        
-        @maquinaSiguiente    =  maquinaSiguiente
-        @productoAnterior    =  productoAnterior
-        @productoProcesado   =  productoProcesado
-        
-        #--- Variables de instancia opcionales al invocar el constructor
-        @estado              =  "Inactiva"
-        @cicloActual         =  ciclosProcesamiento
-        @productoHecho            =  0.0
-        @productoAnteriorRestante =  0.0
-    end
-
+module MaquinaCompleja
     #---------- Metodo que ejecuta un ciclo de procesamiento ----------
     def procesamiento
     
@@ -190,42 +144,5 @@ class Maquina
     end
 #--Fin de la clase Maquina
 end
-
-
-
-
-
-
-numCiclos = ARGV[0]
-cebada    = ARGV[1]
-arrozMaiz = ARGV[2]
-levadura  = ARGV[3]
-lupulo    = ARGV[4]
-
-
-#FALTA PONER LAS MAQUINAS SIGUIENTES
-
-#silosCebada    = MaquinaCompleja.new
-tanque1 = Producto.new(0)
-#pailaMezcla    = MaquinaCompleja.new
-tanque2 = Producto.new(0)
-#pailaCoccion   = MaquinaCompleja.new
-tanque3 = Producto.new(0)
-tanque4 = Producto.new(0)
-#tcc            = MaquinaCompleja.new
-tanque5 = Producto.new(0)
-tanque6 = Producto.new(0)
-productoFinal = Producto.new(0)
-
-
-llenaTapa      = Maquina.new("Llenadora y Tapadora", 0.0, 50, 100, 2, nil, tanque6, productoFinal)
-tanqueFiltrada = Maquina.new("Tanque para Cerveza Filtrada", 0.0, 100, 100, 0, llenaTapa, tanque5, tanque6)
-filtroCerveza  = Maquina.new("Filtro de Cerveza", 0.0, 100, 100, 1, tanqueFiltrada, tanque4, tanque5)
-enfriador      = Maquina.new("Enfriador", 0.0, 60, 100, 2, filtroCerveza, tanque3, tanque4)
-tanque         = Maquina.new("Tanque Pre-Clarificador", 0.01, 35, 100, 1, enfriador, tanque2, tanque3)
-cubaFiltracion = Maquina.new("Cuba de Filtracion", 0.35, 135, 100, 2, tanque, tanque1, tanque2)
-molino         = Maquina.new("Molino", 0.02, 100, 100, 1, cubaFiltracion, nil, tanque1)
-
-cubaFiltracion.procesamiento
 
 
